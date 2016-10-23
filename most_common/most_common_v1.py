ebdbc152f133c18b19b4b28c4b14e71e920f2a73
Mepla <s.nasirian@fax.ir>
2016-10-23 17:01:49 +0330

    most_common solved

--- most_common/most_common_v1.py
+++ most_common/most_common_v1.py
@@ -3,10 +3,22 @@ from collections import Counter
 
 def most_common(string):
     c = Counter(string).most_common(3)
-    sorted(c, key=lambda x: x[0])
+    char_to_num = {}
+    num_to_char = {}
     for i in c:
-        print '{} {}'.format(i[0], i[1])
+        char_to_num[i[0]] = i[1]
+        if i[1] not in num_to_char:
+            num_to_char[i[1]] = []
+
+        num_to_char[i[1]].append(i[0])
+
+    all_keys = sorted(num_to_char.keys(), reverse=True)
+    for i in all_keys:
+        for j in sorted(num_to_char[i]):
+            print '{} {}'.format(j, i)
 
 if __name__ == '__main__':
     string = raw_input()
     most_common(string)
+
+# Passed 8/8
